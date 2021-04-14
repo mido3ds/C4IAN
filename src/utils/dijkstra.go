a5c55ea1cb23ffc8139f5c9c42728ed60b810cb5
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-04-14 12:09:33 +0200

    dijkstra shortest paths

--- src/utils/dijkstra.go
+++ src/utils/dijkstra.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"math"
 )
 
@@ -18,4 +19,51 @@ func (g *Graph) Initialize(src int) {
 	g.best = int64(math.MaxInt64)
 	g.nodes[src].weight = 0
 	g.visiting.PushOrdered(&g.nodes[src])
+}
+
+func (g *Graph) BestPath(src, dest int) BestPath {
+	var path []int
+	for curr := g.nodes[dest]; curr.id != src; curr = g.nodes[curr.best_nodes[0]] {
+		path = append(path, curr.id)
+	}
+	path = append(path, src)
+	// reverse path
+	for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
+		path[i], path[j] = path[j], path[i]
+	}
+	return BestPath{g.nodes[dest].weight, path}
+}
+
+func (g *Graph) ShortestPath(src, dest int) (BestPath, error) {
+	// Intialize graph
+	g.Initialize(src)
+	old_curr := -1
+	var curr *Node
+	for g.visiting.length > 0 {
+		curr = g.visiting.PopOrdered()
+		if old_curr == curr.id {
+			continue
+		}
+		old_curr = curr.id
+		if curr.weight >= g.best {
+			continue
+		}
+		for n, dist := range curr.edges {
+			if curr.weight+dist < g.nodes[n].weight {
+				g.nodes[n].weight = curr.weight + dist
+				g.nodes[n].best_nodes[0] = curr.id
+				// if destination update best
+				if n == dest {
+					g.best = curr.weight + dist
+					g.visited_dest = true
+					continue
+				}
+				g.visiting.PushOrdered(&g.nodes[n])
+			}
+		}
+	}
+	if g.visited_dest == false {
+		return BestPath{}, errors.New("No path found")
+	}
+	return g.BestPath(src, dest), nil
 }
