c967162045207056f4e9fd96cffb519e1d6e53ad
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-04-14 04:16:46 +0200

    Add double linked list functions

--- src/utils/double_linked_list.go
+++ src/utils/double_linked_list.go
@@ -10,3 +10,29 @@ type DoubleLinkedList struct {
 	root   ListElement
 	length int
 }
+
+func newDoubleLinkedList() *DoubleLinkedList {
+	var l DoubleLinkedList
+	return l.Clear()
+}
+
+func (l *DoubleLinkedList) Clear() *DoubleLinkedList {
+	l.root.prev = &l.root
+	l.root.next = &l.root
+	l.length = 0
+	return l
+}
+
+func (l *DoubleLinkedList) Front() *ListElement {
+	if l.length > 0 {
+		return l.root.next
+	}
+	return nil
+}
+
+func (l *DoubleLinkedList) Back() *ListElement {
+	if l.length > 0 {
+		return l.root.prev
+	}
+	return nil
+}
