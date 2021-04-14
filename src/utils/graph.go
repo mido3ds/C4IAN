e1f4b4ca5f165d11bde2b00fdaa7f34e120cda5b
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-04-14 12:07:16 +0200

    Add edge to graph function

--- src/utils/graph.go
+++ src/utils/graph.go
@@ -22,7 +22,7 @@ func (g *Graph) FillDefaults(weight int64, best_node int) {
 	}
 }
 
-func (g *Graph) InsertNode() *Node {
+func (g *Graph) InsertNewNode() *Node {
 	for i, node := range g.nodes {
 		if i != node.id {
 			g.nodes[i] = Node{id: i}
@@ -44,9 +44,22 @@ func (g *Graph) InsertNodes(nodes ...Node) {
 	}
 }
 
+func (g *Graph) InsertNode(id int) *Node {
+	g.InsertNodes(Node{id: id})
+	return &g.nodes[id]
+}
+
 func (g *Graph) GetNode(id int) (node *Node, err error) {
 	if id >= len(g.nodes) {
 		return nil, errors.New("Node is not found")
 	}
 	return &g.nodes[id], nil
+}
+
+func (g *Graph) AddEdge(node1, node2 int, weight int64) error {
+	if len(g.nodes) <= node1 || len(g.nodes) <= node2 {
+		return errors.New("source or destination not found")
+	}
+	g.nodes[node1].AddEdge(node2, weight)
+	return nil // no errors
 }
