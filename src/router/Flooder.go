ac863b05433e45ec4010a8d7d56dba093fce1bc5
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-04-16 17:36:26 +0200

    Refactored Flooder

--- src/router/Flooder.go
+++ src/router/Flooder.go
@@ -8,95 +8,6 @@ import (
 	"github.com/mdlayher/ethernet"
 )
 
-type Flooder struct {
-	seqNumber uint32
-	fTable    *FloodingTable
-	router    *Router
-	macConn   *MACLayerConn
-}
-
-func NewFlooder(router *Router) (*Flooder, error) {
-	// connect to mac layer
-	macConn, err := NewMACLayerConn(router.iface)
-	if err != nil {
-		return nil, err
-	}
-
-	fTable := NewFloodingTable()
-
-	log.Println("initalized flooder")
-
-	return &Flooder{
-		seqNumber: 0,
-		fTable:    fTable,
-		router:    router,
-		macConn:   macConn,
-	}, nil
-}
-
-// seq (4 Bytes) + srcIP (4 Bytes) + packet
-func (flooder *Flooder) Flood(msg []byte) {
-	hdr := FloodHeader{SrcIP: flooder.router.ip, SeqNum: flooder.seqNumber}
-	msg = append(hdr.MarshalBinary(), msg...)
-
-	flooder.seqNumber++
-
-	// add ZID Header
-	zid := &ZIDHeader{zLen: 1, packetType: FloodPacket, srcZID: 2, dstZID: 3}
-	msg = append(zid.MarshalBinary(), msg...)
-
-	encryptedPacket, err := flooder.router.msec.Encrypt(msg)
-	if err != nil {
-		log.Fatal("failed to encrypt packet, err: ", err)
-	}
-
-	err = flooder.macConn.Write(encryptedPacket, ethernet.Broadcast)
-	if err != nil {
-		log.Fatal("failed to write to the device driver: ", err)
-	}
-}
-
-func (flooder *Flooder) ReceiveFloodedMsg(msg []byte) {
-	hdr, ok := UnpackFloodHeader(msg)
-	if !ok {
-		return
-	}
-
-	if bytes.Equal(hdr.SrcIP, flooder.router.ip) {
-		log.Println("My flooded msg returned to me")
-		return
-	}
-
-	log.Println("I received a msg from ", net.IP(hdr.SrcIP))
-
-	tableSeq, exist := flooder.fTable.Get(hdr.SrcIP)
-
-	log.Println("Seq Number: ", hdr.SeqNum)
-	log.Println("Exist: ", exist)
-	log.Println("Table seq: ", tableSeq)
-
-	if exist && tableSeq <= hdr.SeqNum {
-		return
-	}
-
-	flooder.fTable.Set(hdr.SrcIP, hdr.SeqNum)
-
-	// add ZID Header
-	zid := &ZIDHeader{zLen: 1, packetType: FloodPacket, srcZID: 2, dstZID: 3}
-	msg = append(zid.MarshalBinary(), msg...)
-
-	// encrypt the msg
-	encryptedPacket, err := flooder.router.msec.Encrypt(msg)
-	if err != nil {
-		log.Fatal("failed to encrypt packet, err: ", err)
-	}
-
-	// reflood the msg
-	err = flooder.macConn.Write(encryptedPacket, ethernet.Broadcast)
-	if err != nil {
-		log.Fatal("failed to write to the device driver: ", err)
-	}
-}
 
 type FloodHeader struct {
 	// [0:2] checksum here
@@ -142,3 +53,94 @@ func (f *FloodHeader) MarshalBinary() []byte {
 
 	return header[:]
 }
+
+type Flooder struct {
+	seqNumber uint32
+	fTable    *FloodingTable
+	router    *Router
+	macConn   *MACLayerConn
+}
+
+
+func NewFlooder(router *Router) (*Flooder, error) {
+	// connect to mac layer
+	macConn, err := NewMACLayerConn(router.iface)
+	if err != nil {
+		return nil, err
+	}
+
+	fTable := NewFloodingTable()
+
+	log.Println("initalized flooder")
+
+	return &Flooder{
+		seqNumber: 0,
+		fTable:    fTable,
+		router:    router,
+		macConn:   macConn,
+	}, nil
+}
+
+func (flooder *Flooder) Flood(msg []byte) {
+	hdr := FloodHeader{SrcIP: flooder.router.ip, SeqNum: flooder.seqNumber}
+	msg = append(hdr.MarshalBinary(), msg...)
+
+	flooder.seqNumber++
+
+	// add ZID Header
+	zid := &ZIDHeader{zLen: 1, packetType: FloodPacket, srcZID: 2, dstZID: 3}
+	msg = append(zid.MarshalBinary(), msg...)
+
+	encryptedPacket, err := flooder.router.msec.Encrypt(msg)
+	if err != nil {
+		log.Fatal("failed to encrypt packet, err: ", err)
+	}
+
+	err = flooder.macConn.Write(encryptedPacket, ethernet.Broadcast)
+	if err != nil {
+		log.Fatal("failed to write to the device driver: ", err)
+	}
+}
+
+func (flooder *Flooder) ReceiveFloodedMsg(msg []byte) {
+	hdr, ok := UnpackFloodHeader(msg)
+	if !ok {
+		return
+	}
+
+	if bytes.Equal(hdr.SrcIP, flooder.router.ip) {
+		log.Println("My flooded msg returned to me")
+		return
+	}
+
+	log.Println("I received a msg from ", net.IP(hdr.SrcIP))
+
+	tableSeq, exist := flooder.fTable.Get(hdr.SrcIP)
+
+	log.Println("Seq Number: ", hdr.SeqNum)
+	log.Println("Exist: ", exist)
+	log.Println("Table seq: ", tableSeq)
+
+	if exist && hdr.SeqNum <= tableSeq {
+		return
+	}
+
+	flooder.fTable.Set(hdr.SrcIP, hdr.SeqNum)
+
+	// add ZID Header
+	zid := &ZIDHeader{zLen: 1, packetType: FloodPacket, srcZID: 2, dstZID: 3}
+	msg = append(zid.MarshalBinary(), msg...)
+
+	// encrypt the msg
+	encryptedPacket, err := flooder.router.msec.Encrypt(msg)
+	if err != nil {
+		log.Fatal("failed to encrypt packet, err: ", err)
+	}
+
+	// reflood the msg
+	err = flooder.macConn.Write(encryptedPacket, ethernet.Broadcast)
+	if err != nil {
+		log.Fatal("failed to write to the device driver: ", err)
+	}
+}
+
