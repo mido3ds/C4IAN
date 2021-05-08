9440b1ce0c9cfbc54fcc17a43da05f91fa034abf
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-05-08 18:11:48 +0200

    Add timers queue to cache

--- src/router/odmrp/cache.go
+++ src/router/odmrp/cache.go
@@ -4,25 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/cornelk/hashmap"
 	. "github.com/mido3ds/C4IAN/src/router/ip"
+	. "github.com/mido3ds/C4IAN/src/router/tables"
 )
+
+const cacheTimeout = 960 * time.Millisecond
 
 type cacheEntry struct {
 	seqNo   uint64
 	grpIP   net.IP
 	prevHop net.HardwareAddr
 	cost    int8
+	timer   Timer
 }
 
 type cache struct {
-	m *hashmap.HashMap
+	m      *hashmap.HashMap
+	timers *TimersQueue
 }
 
-func newCache() *cache {
+func newCache(timers *TimersQueue) *cache {
 	return &cache{
-		m: &hashmap.HashMap{},
+		m:      &hashmap.HashMap{},
+		timers: timers,
 	}
 }
 
@@ -41,14 +48,16 @@ func (f *cache) set(src net.IP, entry *cacheEntry) bool {
 		log.Panic("you can't enter nil entry")
 	}
 	v, ok := f.m.Get(IPv4ToUInt32(src))
+	val := v.(*cacheEntry)
 	if ok {
-		val := v.(*cacheEntry)
-		if val.cost <= entry.cost && val.seqNo < entry.seqNo {
-			f.m.Set(IPv4ToUInt32(src), entry)
-			return true
+		if !(val.cost <= entry.cost && val.seqNo < entry.seqNo) {
+			return false
 		}
-		return false
 	}
+	val.timer.Stop()
+	entry.timer = f.timers.Add(cacheTimeout, func() {
+		f.del(src)
+	})
 	f.m.Set(IPv4ToUInt32(src), entry)
 	return true
 }
