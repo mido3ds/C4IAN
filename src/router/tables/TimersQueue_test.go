fee1dfcc85a9d0ac7f4f43b5639f31ab58416f1a
Mahmoud Adas <mido3ds@gmail.com>
2021-05-08 07:25:05 +0200

    timersqueue: can stop and reset timers

--- src/router/tables/TimersQueue_test.go
+++ src/router/tables/TimersQueue_test.go
@@ -10,18 +10,28 @@ func TestTimersQueue(t *testing.T) {
 	go tim.Start()
 
 	t2 := time.Now()
-	tim.Add(2*time.Second, func() {
-		t.Log(1, time.Since(t2), "s")
-	})
+
 	tim.Add(1*time.Second, func() {
 		t.Log(2, time.Since(t2), "s")
 	})
-	tim.Add(1*time.Second, func() {
+
+	t1 := tim.Add(2*time.Second, func() {
+		t.Error("shouldnt see this log")
+	})
+	if !t1.Stop() {
+		t.Error("couldnt stop timer")
+	}
+
+	t3 := tim.Add(2*time.Second, func() {
 		t.Log(3, time.Since(t2), "s")
 	})
+	if !t3.Reset(5 * time.Second) {
+		t.Error("couldnt reset timer")
+	}
+
 	tim.Add(3*time.Second, func() {
 		t.Log(4, time.Since(t2), "s")
 	})
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(9 * time.Second)
 }
