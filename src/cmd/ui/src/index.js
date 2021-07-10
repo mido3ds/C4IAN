e6fa32bb51d7b2ce5c637ee24e30f032c2b96c2f
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-10 23:03:01 +0200

    cmd-ui: removed unrequired code, add needed scripts and fonts

--- src/cmd/ui/src/index.js
+++ src/cmd/ui/src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <h1> hello </h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+  
+
