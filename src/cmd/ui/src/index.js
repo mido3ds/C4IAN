35e6f69f211f100836bb5812c2d6bb079dfa8214
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-12 18:40:38 +0200

    cmd-ui: bind map popup with selected unit & add animation for units movement

--- src/cmd/ui/src/index.js
+++ src/cmd/ui/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Profile from './Profile/Profile'
 import Home from './Home/Home'
+//import Test from './Test/Test'
 import './index.css';
 import './App.css';
 
