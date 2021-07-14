afbd4c14c445345a6e30e46f967a73c4d162667b
Yosry Ahmed <yosrym93@gmail.com>
2021-07-14 18:46:14 +0200

    Added event types to models

--- src/models/models.go
+++ src/models/models.go
@@ -1,5 +1,16 @@
 package models
 
+const (
+	MESSAGE_EVENT      = "msg"
+	AUDIO_EVENT        = "audio"
+	VIDEO_FRAME_EVENT  = "video-frame"
+	SENSORS_DATA_EVENT = "sensors-data"
+)
+
+type Event interface {
+	eventType() string
+}
+
 // Time as integer to store in db as Unix Time (SQLite3 does not support a time type)
 type Message struct {
 	Time int `json:"time" db:"time"`
@@ -26,4 +37,20 @@ type SensorData struct {
 	Heartbeat int `json:"heartbeat" db:"heartbeat"`
 	Loc_x     int `json:"loc_x" db:"loc_x"`
 	Loc_y     int `json:"loc_y" db:"loc_y"`
+}
+
+func (msg *Message) eventType() string {
+	return MESSAGE_EVENT
+}
+
+func (audio *Audio) eventType() string {
+	return AUDIO_EVENT
+}
+
+func (frame *VideoFrame) eventType() string {
+	return VIDEO_FRAME_EVENT
+}
+
+func (sensors *SensorData) eventType() string {
+	return SENSORS_DATA_EVENT
 }
