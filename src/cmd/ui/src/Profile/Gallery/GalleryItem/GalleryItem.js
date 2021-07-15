21cdd74fe4aa3f87af5142f65c0d13ae470e4158
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-15 12:01:03 +0200

    cmd-ui: make gallery item component generic for both audio and video

--- src/cmd/ui/src/Profile/Gallery/GalleryItem/GalleryItem.js
+++ src/cmd/ui/src/Profile/Gallery/GalleryItem/GalleryItem.js
@@ -5,15 +5,19 @@ import PlayAudio from '../../../PlayAudio/PlayAudio'
 
 function GalleryItem({type, data, time, name}) {
     const playAudioRef = useRef(null);
-    var playAudio = (audio) => {
-        playAudioRef.current.openModal()
+    var playMedia = (audio) => {
+        if(type === "audio") {
+            playAudioRef.current.openModal()
+        } else if(type === "audio") {
+            
+        }
     }
     
     return (
         <>
         <PlayAudio name={name} audioBolb={data} ref={playAudioRef}></PlayAudio>
         <div  data-augmented-ui="border" className="gallery-item">
-            <i onClick={playAudio} className="fas fa-play-circle fa-3x gallery-item-play-icon"></i>
+            <i onClick={playMedia} className="fas fa-play-circle fa-3x gallery-item-play-icon"></i>
             <Moment className="gallery-item-time" format="wo MMM hh:mm" unix>{time}</Moment>
         </div>
         </>
