48324f50a54a3e1034371ce55427680263b8e817
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-15 11:58:38 +0200

    cmd-ui: implemented frontend api requests using axios

---
+++ src/cmd/ui/src/Api/Api.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3170/api/";
+
+export function postMsg(ip, msg) {
+    axios.post(baseURL + "msgs"+ ip, msg).then((response) => {
+        return response;
+    });
+}
+
+export function postAudioMsg(ip, audio) {
+    axios.post(baseURL + "audio-msgs"+ ip, audio).then((response) => {
+        return response;
+    });
+}
+
+export function getAudioMsgs(ip) {
+    axios.get(baseURL + "audio-msgs/"+ ip).then((response) => {
+        return response.data;
+    });
+}
+
+export function getMsgs(ip) {
+    axios.get(baseURL + "msgs"+ ip).then((response) => {
+        return response.data;
+    });
+}
+
+export function getVideos(ip) {
+    axios.get(baseURL + "videos"+ ip).then((response) => {
+        return response.data;
+    });
+}
+
+export function getSensorsData(ip) {
+    axios.get(baseURL + "videos"+ ip).then((response) => {
+        return response.data;
+    });
+}
+
+export function getLocationHistory(ip) {
+    axios.get(baseURL + "videos"+ ip).then((response) => {
+        return response.data;
+    });
+}
+
+function getHeartBeatHistory(ip) {
+    axios.get(baseURL + "videos"+ ip).then((response) => {
+        return response.data;
+    });
+}
