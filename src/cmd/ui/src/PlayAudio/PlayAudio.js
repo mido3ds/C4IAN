f3ed8571f1108dafcb838d049d7156382134a623
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-15 11:19:53 +0200

    cmd-ui: fixed play audio close button css typo

---
+++ src/cmd/ui/src/PlayAudio/PlayAudio.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import './PlayAudio.css'
+
+import ReactAudioPlayer from 'react-audio-player'
+import blobToBuffer from 'blob-to-buffer'
+
+Modal.setAppElement('#root');
+
+class PlayAudio extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            audioUrl: null,
+            isOpen: false,
+        }
+    }
+
+    openModal = () => {
+        this.setState({
+            isOpen: true
+        })
+    }
+
+    closeModal = () => {
+        this.setState({
+            isOpen: false
+        })
+    }
+
+    onRecevingVideo = (blob) => {
+        blobToBuffer(blob, (err, buffer) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+
+            this.setState({
+                audioBolb: blob
+            })
+
+            if (this.state.audioUrl) {
+                window.URL.revokeObjectURL(this.state.audioUrl)
+            }
+
+            this.setState({
+                audioUrl: window.URL.createObjectURL(blob)
+            })
+        })
+    }
+
+    componentDidMount() {
+        //this.onRecevingVideo(this.props.audioBolb)
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Modal
+                    isOpen={this.state.isOpen}
+                    onRequestClose={this.closeModal}
+                    className="play-audio-modal">
+                    <button className="close" onClick={this.closeModal}>
+                        &times;
+                    </button>
+                    <p className="play-audio-msg"> {this.props.name + "'s audio message"}. </p>
+                    {this.state.audioUrl ?
+                        <ReactAudioPlayer
+                            id='audio'
+                            controls
+                            className="audio-player"
+                            src={this.state.audioUrl}
+                        ></ReactAudioPlayer>
+
+                        : <> </>
+                    }
+                </Modal>
+            </div>
+        );
+    }
+
+} export default PlayAudio;
