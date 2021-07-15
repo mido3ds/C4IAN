73ce19bdeba0560c01d8ae06391ab9401e567247
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-15 11:17:48 +0200

    cmd-ui: added gallery data and tested with real backend values

--- src/cmd/ui/src/Profile/Gallery/Gallery.js
+++ src/cmd/ui/src/Profile/Gallery/Gallery.js
@@ -1,21 +1,46 @@
 import './Gallery.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GalleryItem from './GalleryItem/GalleryItem'
 import Pagination from './Pagination/Pagination'
+import { getAudioMsgs, getVideos } from '../../Api/Api'
+import { media } from "../../media";
 
 function Gallery({type, unit}) {
+    const [data, setData] = useState(media);
+    const [startItem, setStartItem] = useState(0)
+
+    var paginate = (pageNumber) => {
+        setStartItem(4 * (pageNumber - 1));
+    }
+
+    useEffect(() => {
+        if(type === "audio") {
+            //setData(getAudioMsgs(unit.ip))
+        } else if (type === "video") {
+            //setData(getVideos(unit.ip))
+        }
+    })
+
     return (
         <div className="gallery-container">
-            <div className="items-container">
-                <GalleryItem />
-                <GalleryItem />
-                <GalleryItem />
-                <GalleryItem />
-            </div>
-            <Pagination
-                paginate={5}
-                hasNext={true}
-            />
+            {!data || !data.length ?
+            <div className="no-data-gallery-msg"> 
+                <p> No data to be previewed </p> 
+            </div>: 
+            <> 
+                <div className="items-container">
+                    {
+                        [...Array(Math.min(data.length - startItem, 4))].map((x, i) => 
+                            <GalleryItem name= {unit.name} type="type" time={data[i + startItem].time} data={data[i].body} key={i} />
+                        )
+                    }
+                </div>
+                <Pagination
+                    dataSize={data.length}
+                    paginate={paginate}
+                />
+            </>
+        }
         </div>
     );
 }
--- src/cmd/ui/src/Profile/Gallery/GalleryItem/GalleryItem.js
+++ src/cmd/ui/src/Profile/Gallery/GalleryItem/GalleryItem.js
@@ -1,10 +1,22 @@
 import './GalleryItem.css';
-import React from 'react';
+import React, { useRef } from 'react';
+import Moment from 'react-moment';
+import PlayAudio from '../../../PlayAudio/PlayAudio'
 
-function Gallery() {
+function GalleryItem({type, data, time, name}) {
+    const playAudioRef = useRef(null);
+    var playAudio = (audio) => {
+        playAudioRef.current.openModal()
+    }
+    
     return (
-        <div data-augmented-ui="border" className="gallery-item">
+        <>
+        <PlayAudio name={name} audioBolb={data} ref={playAudioRef}></PlayAudio>
+        <div  data-augmented-ui="border" className="gallery-item">
+            <i onClick={playAudio} className="fas fa-play-circle fa-3x gallery-item-play-icon"></i>
+            <Moment className="gallery-item-time" format="wo MMM hh:mm" unix>{time}</Moment>
         </div>
+        </>
     );
 }
-export default Gallery;
+export default GalleryItem;
