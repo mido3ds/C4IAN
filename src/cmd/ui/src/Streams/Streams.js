05d5d8b97799b9a54507280b22bcfbd75386383a
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-16 14:41:48 +0200

    cmd-ui: bind video streams with back SSE

--- src/cmd/ui/src/Streams/Streams.js
+++ src/cmd/ui/src/Streams/Streams.js
@@ -4,22 +4,40 @@ import Stream from './Stream/Stream'
 
 class Streams extends React.Component {
 
+    onReceiveFragment(data) {
+        if(this.state.Streams.some(e => e.ID === data.ID)) {
+            // Add Fragment
+        } else {
+            Streams.push({ID: Stream.ID})
+        }
+    }
+
     componentDidMount() {
         var number = window.$('.stream').length;
         var columns = Math.ceil(Math.sqrt(number) - 0.1);
         var rows = Math.ceil(number / columns);
         window.$('.stream').css("width", `${100 / columns}%`);
         window.$('.stream').css("height", `${100 / rows}%`);
-    }
 
+        this.state = {
+            eventSource: new EventSource("http://localhost:3170/events"),
+            Streams : []
+        }
+
+        this.state.eventSource.addEventListener("video-fragment", ev => {
+            this.onReceiveFragment(JSON.parse(ev.data))
+        })
+    }
 
     render() {
         return (
             <div className="video-root">
                 <div className="streams-container">
-                    <div className="stream">
-                        <Stream></Stream>
-                    </div>
+                    {Streams.map((value, index) => {
+                        return <div className="stream">
+                                     <Stream streamID={value.ID}> </Stream>
+                                </div>
+                    })}
                 </div>
             </div>
         );
