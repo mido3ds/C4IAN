0395e6d330b9b9d3c6313c6cf6700663866a2030
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-17 16:59:50 +0200

    cmd-ui: modified api requests to await response

--- src/cmd/ui/src/Api/Api.js
+++ src/cmd/ui/src/Api/Api.js
@@ -2,56 +2,46 @@ import axios from "axios";
 
 const baseURL = "http://localhost:3170/api/";
 
-export function postMsg(ip, msg) {
-    axios.post(baseURL + "msg/"+ ip, msg).then((response) => {
-        return response;
-    });
+export async function postMsg(ip, msg) {
+    axios.post(baseURL + "msg/"+ ip, msg);
 }
 
-export function postAudioMsg(ip, audio) {
-    axios.post(baseURL + "audio-msg/"+ ip, audio).then((response) => {
-        return response;
-    });
+export async function postAudioMsg(ip, audio) {
+    var response = await axios.post(baseURL + "audio-msg/"+ ip, audio);
+    return response.data;
 }
 
-export function getUnits() {
-    axios.get(baseURL + "units/").then((response) => {
-        return response.data;
-    });
+export async function getUnits() {
+    var response = await axios.get(baseURL + "units");
+    return response.data;
 }
 
-export function getGroups() {
-    axios.get(baseURL + "groups/").then((response) => {
-        return response.data;
-    });
+export async function getGroups() {
+    var response = await axios.get(baseURL + "groups");
+    return response.data;
 }
 
-export function getMembers() {
-    axios.get(baseURL + "groups/").then((response) => {
-        return response.data;
-    });
+export async function getMembers() {
+    var response = await axios.get(baseURL + "memberships");
+    return response.data;
 }
 
-export function getAudioMsgs(ip) {
-    axios.get(baseURL + "audio-msgs/"+ ip).then((response) => {
-        return response.data;
-    });
+export async function getAudioMsgs(ip) {
+    var response = await axios.get(baseURL + "audio-msgs/"+ ip);
+    return response.data;
 }
 
-export function getMsgs(ip) {
-    axios.get(baseURL + "msgs"+ ip).then((response) => {
-        return response.data;
-    });
+export async function getMsgs(ip) {
+    var response = await axios.get(baseURL + "msgs/"+ ip);
+    return response.data;
 }
 
-export function getVideos(ip) {
-    axios.get(baseURL + "videos"+ ip).then((response) => {
-        return response.data;
-    });
+export async function getVideos(ip) {
+    var response = await axios.get(baseURL + "videos/"+ ip);
+    return response.data;
 }
 
-export function getSensorsData(ip) {
-    axios.get(baseURL + "videos"+ ip).then((response) => {
-        return response.data;
-    });
+export async function getSensorsData(ip) {
+    var response = await axios.get(baseURL + "sensors-data/"+ ip);
+    return response.data;
 }
