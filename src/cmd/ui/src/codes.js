8a3523649861641861cafc066ef4d0f69cc597ef
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-17 17:05:43 +0200

    cmd-ui: changed messages codes between cmd and units

--- src/cmd/ui/src/codes.js
+++ src/cmd/ui/src/codes.js
@@ -1,7 +1,7 @@
-const codes = {
+const receivedCodes = {
     0: "We are in danger, please send help",
     1: "We did it!!",
     2: "We need supplies as soon as possible",
     3: "Plan must be cahnged immediately",
 }
-export { codes }
+export { receivedCodes }
