038ab15a6210feb58c39675263577c8027bfc0c5
Yosry Ahmed <yosrym93@gmail.com>
2021-07-17 16:42:13 +0200

    CMD Daemon: Changed CORS handler

--- src/cmd/daemon/api.go
+++ src/cmd/daemon/api.go
@@ -7,9 +7,9 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/mido3ds/C4IAN/src/models"
-	"github.com/rs/cors"
 	"gopkg.in/antage/eventsource.v1"
 )
 
@@ -52,7 +52,7 @@ func (api *API) Start(port int, unitsPort int, dbManager *DatabaseManager, netMa
 
 	// Listen for HTTP requests
 	address := ":" + strconv.Itoa(port)
-	log.Fatal(http.ListenAndServe(address, cors.Default().Handler(router)))
+	log.Fatal(http.ListenAndServe(address, handlers.CORS()(router)))
 }
 
 func (api *API) SendEvent(body models.Event) {
--- src/cmd/daemon/go.mod
+++ src/cmd/daemon/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/cornelk/hashmap v1.0.1
 	github.com/dchest/siphash v1.2.2 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.10.2 // indirect
