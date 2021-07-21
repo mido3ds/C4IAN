e2f945dfed8e26c29a0dcc0045e55b5fbf811ac0
abdulrahman-khalid <abdulrahman.elshafei98@gmail.com>
2021-07-21 20:38:31 +0200

    change ui port to 3006 in unit ui

--- src/unit/ui/src/electron-starter.js
+++ src/unit/ui/src/electron-starter.js
@@ -13,10 +13,10 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-
+    mainWindow = new BrowserWindow();
+    mainWindow.maximize()
     // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL('http://localhost:3006');
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
