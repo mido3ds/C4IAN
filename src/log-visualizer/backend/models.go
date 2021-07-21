6402c998e53d4c4808b3e6061f18447ab0dff652
Yosry Ahmed <yosrym93@gmail.com>
2021-07-21 17:06:00 +0200

    Log-Visualizer: implmplemented backend to fetch data from the database and serve it to the UI

---
+++ src/log-visualizer/backend/models.go
@@ -0,0 +1,17 @@
+package main
+
+type Packet struct {
+	StartTime int64  `json:"start" db:"start_time"`
+	Dst       string `json:"dst" db:"dst"`
+	Hash      []byte `json:"hash" db:"packet_hash"`
+}
+
+type Location struct {
+	Lat float64 `json:"lat" db:"lat"`
+	Lon float64 `json:"lon" db:"lon"`
+}
+
+type PacketData struct {
+	Locations map[string]Location `json:"locations"`
+	Path      []string            `json:"path"`
+}
