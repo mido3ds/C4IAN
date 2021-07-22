5e9af62616b33b4e5b24cf4acbbc6fa4aaf11e15
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-07-22 18:30:22 +0200

    ui: fixed code message typo

--- src/cmd/ui/src/codes.js
+++ src/cmd/ui/src/codes.js
@@ -2,6 +2,6 @@ const receivedCodes = {
     0: "We are in danger, please send help",
     1: "We did it!!",
     2: "We need supplies as soon as possible",
-    3: "Plan must be cahnged immediately",
+    3: "Plan must be changed immediately",
 }
 export { receivedCodes }
